a09d45eeb88fa019547f26a7674c1d1e65a64192
Andrew Growney <ajgrowney@gmail.com>
2019-02-27 13:39:39 -0600

    Update: main changes

--- main.py
+++ main.py
@@ -44,108 +44,90 @@ def run_matchFilter(plot=False, process_len=100, num_cores=cpu_count()):
     # Get a unique list of stations
     stations = list(set(stations))
 
-    # We will loop through the data chunks at a time, these chunks can be any
-    # size, in general we have used 1 day as our standard, but this can be
-    # as short as five minutes (for MAD thresholds) or shorter for other
-    # threshold metrics. However the chunk size should be the same as your
-    # template process_len.
-
-    # You should test different parameters!!!
-    start_time = UTCDateTime(2018, 1, 1, 00)
-    end_time = UTCDateTime(2018, 1, 1, 14)
-    chunks = []
-    chunk_start = start_time
-    while chunk_start < end_time:
-        chunk_end = chunk_start + process_len
-        if chunk_end > end_time:
-            chunk_end = end_time
-        chunks.append((chunk_start, chunk_end))
-        chunk_start += process_len
-
     unique_detections = []
 
     # Note that these chunks do not rely on each other, and could be paralleled
     # on multiple nodes of a distributed cluster, see the SLURM tutorial for
     # an example of this.
-    for t1, t2 in chunks:
-        # Generate the bulk information to query the GeoNet database
-        t1Folder = str(t1.year) + "_" + (str(t1.month).zfill(2)) # '2018_01'
-        t2Folder = str(t2.year) + "_" + (str(t2.month).zfill(2)) # '2018_01'
+    # for t1, t2 in chunks:
+    #     # Generate the bulk information to query the GeoNet database
+    #     t1Folder = str(t1.year) + "_" + (str(t1.month).zfill(2)) # '2018_01'
+    #     t2Folder = str(t2.year) + "_" + (str(t2.month).zfill(2)) # '2018_01'
+    #
+    #     bulk_info = []
+    #     for station in stations:
+    #         print("Station",type(station))
+    #         print("Time",type(t1),t2)
+    #         print("Month t1", t1.minute, t2.minute)
+    #         print(t1Folder)
+    #         print(station)
+    #         print(station[0])
+    #         print(station[1][-1])
+    #         bulk_info.append(())
+    #         return
 
-        bulk_info = []
-        for station in stations:
-            print("Station",type(station))
-            print("Time",type(t1),t2)
-            print("Month t1", t1.minute, t2.minute)
-            print(t1Folder)
-            print(station)
-            print(station[0])
-            print(station[1][-1])
-            bulk_info.append(())
-            return
+    # DONE: Find effective method to get our waveforms in bulk
+    # Note this will take a little while.
+    print('Downloading seismic data locally from 2018_01')
+    streams = get_waveforms_bulk("2018_01")
 
-        # TODO: Find effective method to get our waveforms in bulk
-        # Note this will take a little while.
-        print('Downloading seismic data, this may take a while')
-        st = client.get_waveforms_bulk(bulk_info)
-        print(type(st))
-        return
 
-        # TODO: Do we need to merge the stream
-        # Merge the stream, it will be downloaded in chunks
+    # DONE: Do we need to merge the stream
+    # Merge the stream, it will be downloaded in chunks
+    for st in streams:
         st.merge(fill_value='interpolate')
 
-        # Pre-process the data to set frequency band and sampling rate
-        # Note that this is, and MUST BE the same as the parameters used for
-        # the template creation.
-        print('Processing the seismic data')
-        st = pre_processing.shortproc(
-            st, lowcut=2.0, highcut=9.0, filt_order=4, samp_rate=20.0,
-            debug=0, num_cores=num_cores, starttime=t1, endtime=t2)
-        # Convert from list to stream
-        st = Stream(st)
+    # Pre-process the data to set frequency band and sampling rate
+    # Note that this is, and MUST BE the same as the parameters used for
+    # the template creation.
+    print('Processing the seismic data')
+    st = pre_processing.shortproc(
+        st, lowcut=2.0, highcut=9.0, filt_order=4, samp_rate=20.0,
+        debug=0, num_cores=num_cores, starttime=t1, endtime=t2)
+    # Convert from list to stream
+    st = Stream(st)
 
-        # Now we can conduct the matched-filter detection
-        detections = match_filter.match_filter(
-            template_names=template_names, template_list=templates,
-            st=st, threshold=8.0, threshold_type='MAD', trig_int=6.0,
-            plotvar=plot, plotdir='.', cores=num_cores, debug=0,
-            plot_format='png')
+    # Now we can conduct the matched-filter detection
+    detections = match_filter.match_filter(
+        template_names=template_names, template_list=templates,
+        st=st, threshold=8.0, threshold_type='MAD', tribulk_infog_int=6.0,
+        plotvar=plot, plotdir='.', cores=num_cores, debug=0,
+        plot_format='png')
 
-        # Now lets try and work out how many unique events we have just to
-        # compare with the GeoNet catalog of 20 events on this day in this
-        # sequence
-        for master in detections:
-            keep = True
-            for slave in detections:
-                if not master == slave and abs(master.detect_time -
-                                               slave.detect_time) <= 1.0:
-                    # If the events are within 1s of each other then test which
-                    # was the 'best' match, strongest detection
-                    if not master.detect_val > slave.detect_val:
-                        keep = False
-                        print('Removed detection at %s with cccsum %s'
-                              % (master.detect_time, master.detect_val))
-                        print('Keeping detection at %s with cccsum %s'
-                              % (slave.detect_time, slave.detect_val))
-                        break
-            if keep:
-                unique_detections.append(master)
-                print('Detection at :' + str(master.detect_time) +
-                      ' for template ' + master.template_name +
-                      ' with a cross-correlation sum of: ' +
-                      str(master.detect_val))
-                # We can plot these too
-                if plot:
-                    stplot = st.copy()
-                    template = templates[template_names.index(
-                        master.template_name)]
-                    lags = sorted([tr.stats.starttime for tr in template])
-                    maxlag = lags[-1] - lags[0]
-                    stplot.trim(starttime=master.detect_time - 10,
-                                endtime=master.detect_time + maxlag + 10)
-                    plotting.detection_multiplot(
-                        stplot, template, [master.detect_time.datetime])
+    # Now lets try and work out how many unique events we have just to
+    # compare with the GeoNet catalog of 20 events on this day in this
+    # sequence
+    for master in detections:
+        keep = True
+        for slave in detections:
+            if not master == slave and abs(master.detect_time -
+                                           slave.detect_time) <= 1.0:
+                # If the events are within 1s of each other then test which
+                # was the 'best' match, strongest detection
+                if not master.detect_val > slave.detect_val:
+                    keep = False
+                    print('Removed detection at %s with cccsum %s'
+                          % (master.detect_time, master.detect_val))
+                    print('Keeping detection at %s with cccsum %s'
+                          % (slave.detect_time, slave.detect_val))
+                    break
+        if keep:
+            unique_detections.append(master)
+            print('Detection at :' + str(master.detect_time) +
+                  ' for template ' + master.template_name +
+                  ' with a cross-correlation sum of: ' +
+                  str(master.detect_val))
+            # We can plot these too
+            if plot:
+                stplot = st.copy()
+                template = templates[template_names.index(
+                    master.template_name)]
+                lags = sorted([tr.stats.starttime for tr in template])
+                maxlag = lags[-1] - lags[0]
+                stplot.trim(starttime=master.detect_time - 10,
+                            endtime=master.detect_time + maxlag + 10)
+                plotting.detection_multiplot(
+                    stplot, template, [master.detect_time.datetime])
     print('We made a total of ' + str(len(unique_detections)) + ' detections')
     return unique_detections
 
@@ -239,7 +221,5 @@ if __name__ == '__main__':
         run_pickAndLag_tutorial(min_magnitude=4, num_cores=cpu_count())
     elif sys.argv[1] == "matchFilter":
         run_matchFilter()
-    elif sys.argv[1] == "getwaveformsbulk":
-        get_single_stream("2018_01","2018-01-01-0000-00M","COMB__033")
     elif sys.argv[1] == "bulk":
         get_waveforms_bulk("2018_01")
